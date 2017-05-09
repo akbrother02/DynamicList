178e3507a6584d8d7d3e099bdafec629ab99839d
Ashish Kumar <akbrother02@gmail.com>
2017-05-09 20:38:39 +0530

    Formatting the List

--- app/src/main/java/loadingdata/ashish/myapplication/AddNewPerson.java
+++ app/src/main/java/loadingdata/ashish/myapplication/AddNewPerson.java
@@ -1,7 +1,5 @@
 package loadingdata.ashish.myapplication;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,81 +10,88 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AddNewPerson extends Activity implements OnClickListener {
 
-	private AtomPayListAdapter adapter;
-	private EditText etname,etaddress;
-	Button sendtoserver;
-	ListView atomPaysListView;
-	ArrayList<AtomPayment> listItems=new ArrayList<>();
-	ArrayAdapter<CharSequence> spinneradapter;
-	private Spinner spinner;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_list_view);
-		etname=(EditText)findViewById(R.id.name);
-		etaddress=(EditText)findViewById(R.id.address);
-		sendtoserver=(Button)findViewById(R.id.senserver);
-		spinner=(Spinner)findViewById(R.id.spinner);
-		setupListViewAdapter();
-		SpinnerSetup();
-		setupAddPaymentButton();
-		sendtoserver.setOnClickListener(this);
-	}
+    Button sendtoserver;
+    ListView atomPaysListView;
+    ArrayList<AtomPayment> listItems;
+    ArrayAdapter<CharSequence> spinneradapter;
+    private AtomPayListAdapter adapter;
+    private EditText etname, etaddress;
+    private Spinner spinner;
 
-	private void SpinnerSetup() {
-		spinneradapter = ArrayAdapter.createFromResource(
-				this, R.array.City, android.R.layout.simple_spinner_item);
-		spinneradapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner.setAdapter(spinneradapter);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+        Widgetinitialization();
 
-	public void removeAtomPayOnClickHandler(View v) {
-		AtomPayment itemToRemove = (AtomPayment)v.getTag();
-		adapter.remove(itemToRemove);
-	}
+        setupListViewAdapter();
+        SpinnerSetup();
+        setupAddPaymentButton();
+        sendtoserver.setOnClickListener(this);
+    }
 
-	private void setupListViewAdapter() {
-		adapter = new AtomPayListAdapter(AddNewPerson.this, R.layout.atom_pay_list_item, new ArrayList<AtomPayment>());
-		atomPaysListView= (ListView)findViewById(R.id.EnterPays_atomPaysList);
-		atomPaysListView.setAdapter(adapter);
-	}
-	
-	private void setupAddPaymentButton() {
-		findViewById(R.id.EnterPays_addAtomPayment).setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				AtomPayment atomPayment=new AtomPayment(etname.getText().toString(),etaddress.getText().toString(),spinner.getSelectedItem().toString());
-				adapter.insert(atomPayment, 0);
-				listItems.add(atomPayment);
-			}
-		});
-	}
+    private void Widgetinitialization() {
+        etname = (EditText) findViewById(R.id.name);
+        etaddress = (EditText) findViewById(R.id.address);
+        sendtoserver = (Button) findViewById(R.id.senserver);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        listItems = new ArrayList<>();
+    }
 
+    private void SpinnerSetup() {
+        spinneradapter = ArrayAdapter.createFromResource(
+                this, R.array.City, android.R.layout.simple_spinner_item);
+        spinneradapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinneradapter);
+    }
 
-	@Override
-	public void onClick(View view) {
-		switch (view.getId())
-		{
-			case R.id.senserver:
-				DataSendtoServer();
-		}
+//    public void removeAtomPayOnClickHandler(View v) {
+//        AtomPayment itemToRemove = (AtomPayment) v.getTag();
+//        adapter.remove(itemToRemove);
+//    }
 
-	}
+    private void setupListViewAdapter() {
+        adapter = new AtomPayListAdapter(AddNewPerson.this, R.layout.atom_pay_list_item, new ArrayList<AtomPayment>());
+        atomPaysListView = (ListView) findViewById(R.id.EnterPays_atomPaysList);
+        atomPaysListView.setAdapter(adapter);
+    }
 
-	private void DataSendtoServer() {
-		for (int i = 0; i <listItems.size() ; i++) {
-			String name=listItems.get(i).getName();
-			String address=listItems.get(i).getAddress();
-			String spinner=listItems.get(i).getSpinner();
-			Log.d("name:",name +" "+address +" "+spinner);
+    private void setupAddPaymentButton() {
+        findViewById(R.id.EnterPays_addAtomPayment).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                AtomPayment atomPayment = new AtomPayment(etname.getText().toString(), etaddress.getText().toString(), spinner.getSelectedItem().toString());
+                adapter.insert(atomPayment, 0);
+                listItems.add(atomPayment);
+            }
+        });
+    }
 
 
-		}
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.senserver:
+                DataSendtoServer();
+        }
 
-	}
+    }
+
+    private void DataSendtoServer() {
+        for (int i = 0; i < listItems.size(); i++) {
+            String name = listItems.get(i).getName();
+            String address = listItems.get(i).getAddress();
+            String spinner = listItems.get(i).getSpinner();
+            Log.d("name:", name + " " + address + " " + spinner);
+
+
+        }
+
+    }
 }
--- app/src/main/java/loadingdata/ashish/myapplication/AtomPayListAdapter.java
+++ app/src/main/java/loadingdata/ashish/myapplication/AtomPayListAdapter.java
@@ -1,106 +1,56 @@
 package loadingdata.ashish.myapplication;
 
-import java.util.List;
-
 import android.app.Activity;
 import android.content.Context;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class AtomPayListAdapter extends ArrayAdapter<AtomPayment> {
 
-	protected static final String LOG_TAG = AtomPayListAdapter.class.getSimpleName();
-	
-	private List<AtomPayment> items;
-	private int layoutResourceId;
-	private Context context;
+    private List<AtomPayment> items;
+    private int layoutResourceId;
+    private Context context;
 
-	public AtomPayListAdapter(Context context, int layoutResourceId, List<AtomPayment> items) {
-		super(context, layoutResourceId, items);
-		this.layoutResourceId = layoutResourceId;
-		this.context = context;
-		this.items = items;
-	}
+    public AtomPayListAdapter(Context context, int layoutResourceId, List<AtomPayment> items) {
+        super(context, layoutResourceId, items);
+        this.layoutResourceId = layoutResourceId;
+        this.context = context;
+        this.items = items;
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View row = convertView;
-		AtomPaymentHolder holder = null;
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        AtomPaymentHolder holder = null;
+        LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+        row = inflater.inflate(layoutResourceId, parent, false);
+        holder = new AtomPaymentHolder();
+        holder.atomPayment = items.get(position);
+        holder.name = (TextView) row.findViewById(R.id.name);
+        holder.address = (TextView) row.findViewById(R.id.address);
+        holder.spinner = (TextView) row.findViewById(R.id.spinner);
 
-		LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-		row = inflater.inflate(layoutResourceId, parent, false);
+        row.setTag(holder);
 
-		holder = new AtomPaymentHolder();
-		holder.atomPayment = items.get(position);
-//		holder.removePaymentButton = (ImageButton)row.findViewById(R.id.atomPay_removePay);
-//		holder.removePaymentButton.setTag(holder.atomPayment);
+        setupItem(holder);
+        return row;
+    }
 
-		holder.name = (TextView)row.findViewById(R.id.name);
-//		setNameTextChangeListener(holder);
-		holder.address = (TextView)row.findViewById(R.id.address);
-		holder.spinner=(TextView)row.findViewById(R.id.spinner);
-//		setValueTextListeners(holder);
+    private void setupItem(AtomPaymentHolder holder) {
+        holder.name.setText(holder.atomPayment.getName());
+        holder.address.setText(holder.atomPayment.getAddress());
+        holder.spinner.setText(holder.atomPayment.getSpinner());
+    }
 
-		row.setTag(holder);
-
-		setupItem(holder);
-		return row;
-	}
-
-	private void setupItem(AtomPaymentHolder holder) {
-		holder.name.setText(holder.atomPayment.getName());
-		holder.address.setText(holder.atomPayment.getAddress());
-		holder.spinner.setText(holder.atomPayment.getSpinner());
-	}
-
-	public static class AtomPaymentHolder {
-		AtomPayment atomPayment;
-		TextView name;
-		TextView address;
-		TextView spinner;
-	}
-	
-//	private void setNameTextChangeListener(final AtomPaymentHolder holder) {
-//		holder.name.addTextChangedListener(new TextWatcher() {
-//
-//			@Override
-//			public void onTextChanged(CharSequence s, int start, int before, int count) {
-//				holder.atomPayment.setName(s.toString());
-//				holder.atomPayment
-//			}
-//
-//			@Override
-//			public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-//
-//			@Override
-//			public void afterTextChanged(Editable s) { }
-//		});
-//	}
-
-//	private void setValueTextListeners(final AtomPaymentHolder holder) {
-//		holder.value.addTextChangedListener(new TextWatcher() {
-//
-//			@Override
-//			public void onTextChanged(CharSequence s, int start, int before, int count) {
-//				try{
-//					holder.atomPayment.setValue(Double.parseDouble(s.toString()));
-//				}catch (NumberFormatException e) {
-//					Log.e(LOG_TAG, "error reading double value: " + s.toString());
-//				}
-//			}
-//
-//			@Override
-//			public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-//
-//			@Override
-//			public void afterTextChanged(Editable s) { }
-//		});
-//	}
+    public static class AtomPaymentHolder {
+        AtomPayment atomPayment;
+        TextView name;
+        TextView address;
+        TextView spinner;
+    }
 }
